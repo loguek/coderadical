991fb1c552d02b4f478d95fcc5884c90b7ec919e
Kevin Logue <kevin.logue@gmail.com>
2020-07-28 20:27:22 +0100

    Added css for other pages Added in anchors

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-function updateHeight() {
-    document.documentElement.style.setProperty('--vh', `${window.innerHeight / 100}px`);
-}
-
-window.addEventListener('resize', () => updateHeight());
-window.addEventListener('orientationchange', () => updateHeight());
-
-updateHeight();
